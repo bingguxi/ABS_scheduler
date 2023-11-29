2c68252bea2e4b84dfc4a2eb0cbd592c778312be
bingguxi <kongsuni3685@gmail.com>
2023-11-30 02:34:56 +0900

    231129 지진 수정 및 스케줄러 설정

--- src/main/java/kopo/poly/dto/EarthquakeLiveDTO.java
+++ src/main/java/kopo/poly/dto/EarthquakeLiveDTO.java
@@ -9,18 +9,23 @@ import lombok.ToString;
 @ToString
 public class EarthquakeLiveDTO {
 
-    /* 지진 실시간 API 데이터를 담을 DTO ( php 크롤링 )*/
+    /* 지진 실시간 API 데이터를 담을 DTO ( json 파싱 )*/
 
     private String earthquakeLiveSeq; // 시퀀스
-    private String tp; // 3(국내지진통보), 2(국외지진정보)
-    private String tmFc; // 발표시간
-    private String seq; //발표일련번호
-    private String tmEqkMsc; // 진앙시(년월일시분초)
-    private String mt; // 규모
-    private String lat; // 진앙 위도 (deg.)
-    private String lon; // 진앙 경도 (deg.)
+    private String cnt; // 참조번호
+    private String fcTp; // 통보종류 ( 2 : 국외 / 3 : 국내 / 5 : 국내 ( 재통보 ) / 11 : 국내 조기경보 / 12 : 국외 조기경보 / 13 : 조기경보 정밀분석 )
+    private String img; // 지도 이미지 ( 진앙 위치 이미지 URL - 통보종류 2,3번만 제공 )
+    private String inT; // 진도 (시범서비스)
+    private String lat; // 위도
     private String loc; // 진앙 위치
-    private String scale; // 진도
+    private String lon; // 경도
+    private String mt; // 규모
     private String rem; // 참고사항
-    private String cor; // 수정사항
+    private String stnId; // 지점코드- 108(전국)으로 고정
+    private String tmEqk; // 진앙시 ( 년월일시분초 ) - 지진 발생 시각
+    private String tmFc; // 발표시각 ( 년월일시분 ) - 통보문 발표 시각
+    private String tmSeq; // 발표 일련번호 ( 월별 )
+    private String dep; // 깊이 - 발생깊이 ( km, 시범 운영 )
+    private String existsYn;
+
 }
--- src/main/java/kopo/poly/service/EarthquakeService.java
+++ src/main/java/kopo/poly/service/EarthquakeService.java
@@ -1,25 +1,17 @@
 package kopo.poly.service;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kopo.poly.dto.EarthquakeLiveDTO;
-import kopo.poly.dto.EarthquakeResultDTO;
 import kopo.poly.persistance.mapper.IEarthquakeMapper;
-import kopo.poly.util.CmmUtil;
+import kopo.poly.util.NetworkUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.springframework.transaction.annotation.Transactional;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -30,288 +22,168 @@ public class EarthquakeService {
 
     private final IEarthquakeMapper earthquakeMapper;
 
-    @Value("${bhg.api.key}")
-    private String apiKey;
+    // 지진 실시간 데이터 파싱을 위한 공공 데이터 포털 API KEY
+    @Value("${data.api.key}")
+    private String dataApiKey;
 
-    String apiURL = "https://apihub.kma.go.kr/api/typ09/url/eqk/urlNewNotiEqk.do";
+    String apiLiveURL = "http://apis.data.go.kr/1360000/EqkInfoService/getEqkMsg";
 
     /**
-     * 지진 실시간 API 크롤링해서 정보 가져오기
-     * DB에 담아두었다가 새로운 요청이 오면 비우고 다시 DB에 담아서 서비스
+     * 지진 실시간 API JSON 파싱해서 정보 가져오기
+     * DB에 담아두었다가 새로운 요청이 오면 비우고 다시 DB에 담아서 서비스 ( 일단 DB 저장 )
+     * 당일 기준 3일까지의 데이터만 들어옴
      */
-    
-    public List<EarthquakeLiveDTO> getEarthquakeLiveInfo() throws Exception {
-
-        log.info(this.getClass().getName() + ".getEarthquakeLiveInfo Start!!");
+    @Scheduled(cron = "0 1 0 * * *") // 매일 오전 12시 1분 0초마다 실행되게 스케줄러 설정
+    @Transactional
+    void manageEarthquakeLiveInfo() throws Exception {
+        log.info(this.getClass().getName() + ".manageEarthquakeLiveInfo Start!!");
 
         log.info("DB 삭제 시작");
 
         earthquakeMapper.deleteEarthquakeLiveInfo();
 
-
         // 현재 시간을 가져옵니다.
         Date currentDate = new Date();
 
-        // 날짜 및 시간 형식을 설정합니다.
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmm");
+        // Calendar 객체를 생성하고 현재 날짜로 설정합니다.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
 
-        // 현재 시간을 원하는 형식으로 변환합니다.
-        String formattedDate = dateFormat.format(currentDate);
+        // 3일 전의 날짜를 계산합니다. ( 3일전까지의 데이터까지 조회하기 위해 파라미터값으로 3일전 날짜를 생성 )
+        calendar.add(Calendar.DAY_OF_MONTH, -3);
 
-        log.info("현재 시각 : " + formattedDate);
+        // 형식을 설정하고 문자열로 변환합니다. ( 문자열로 변환하여 파라미터값으로 사용가능하게 변경 )
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        String formattedCurrentDate = dateFormat.format(currentDate);
+        String threeDaysAgo = dateFormat.format(calendar.getTime());
 
-        // 변환된 날짜와 시간을 사용하여 URL을 생성합니다.
-        String url = "https://apihub.kma.go.kr/api/typ01/url/eqk_now.php?tm=" + formattedDate /*"201307231215"*/ + "&disp=1&help=0&authKey=" + apiKey;
+        String pageNo = "1";
+        String numOfRows = "100";
+        String dataType = "json";
 
-        // Jsoup 라이브러리를 통해 사이트 접속되면, 그 사이트의 전체 HTML소스 저장할 변수
-        Document doc = null;
+        String apiParam = apiLiveURL + "?serviceKey=" + dataApiKey + "&numOfRows=" + numOfRows + "&pageNo=" + pageNo + "&dataType=" + dataType + "&fromTmFc=" + threeDaysAgo + "&toTmFc=" + formattedCurrentDate;
+        log.info("apiParam : " + apiParam);
 
-        // 사이트 접속
-        doc = Jsoup.connect(url).get();
+        Map<String, String> head = new HashMap<>();
 
-        log.info("doc : " + doc);
+        String json = NetworkUtil.get(apiParam, head);
 
-        Elements element = doc.select("body");
+        log.info("json : " + json);
 
-        log.info("element : " + element);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(json);
 
-        // 지진 정보 가져오기
-        Iterator<Element> earthquake = element.select("body").iterator();
+        // totalCount 값 가져오기 (asText() 메서드로 String으로 가져오기)
+        String totalCount = rootNode.path("response").path("body").path("totalCount").asText();
+        log.info("조회된 지진 실시간 데이터의 totalCount : " + totalCount);
 
-        EarthquakeLiveDTO pDTO = null;
+        // items 노드 가져오기
+        JsonNode itemsNode = rootNode.path("response").path("body").path("items").path("item");
 
+        if (itemsNode.isArray()) {
+            for (JsonNode node : itemsNode) {
+                EarthquakeLiveDTO pDTO = new EarthquakeLiveDTO();
 
-        log.info("earthquake : " + earthquake);
-
-        List<EarthquakeLiveDTO> pList = new ArrayList<>();
-
-        int idx = 0;
-
-        // pre 태그에서 추출한 텍스트를 처리하고 EarthquakeLiveDTO 객체에 값을 담아 리스트에 추가하는 로직 추가
-        while (earthquake.hasNext()) {
-
-            if (idx++ > 20) {
-                break;
-            }
-            pDTO = new EarthquakeLiveDTO();
-
-            log.info("pDTO : " + pDTO);
-
-            // pre 태그에서 추출한 텍스트 한 줄씩 읽어오기
-            String line = CmmUtil.nvl(earthquake.next().text());
-
-            log.info("line : " + line);
-
-            log.info("=의 위치 : " + line.indexOf("="));
-            log.info("\n의 위치 : " + line.indexOf("\\\n,"));
-
-//            line = line.replaceAll("#7777END", "");
-            line = line.substring(63);
-
-            log.info("substring 결과 : " + line);
-
-            String[] lines = line.split("="); // 난 한줄씩
-            String[] earthquakeInfoArray = line.split(",");
-
-
-            log.info("lines : " + lines.length);
-            log.info("earthquakeInfoArray : " + earthquakeInfoArray.length);
-
-
-            // 3번째 줄부터 출력하기
-            for (int i = 0; i < lines.length && (11 + 11 * i) < earthquakeInfoArray.length; i++) {
-                pDTO.setTp(CmmUtil.nvl(earthquakeInfoArray[0 + 11 * i].replaceAll("= ", "")));
-                pDTO.setTmFc(CmmUtil.nvl(earthquakeInfoArray[1 + 11 * i]));
-                pDTO.setSeq(CmmUtil.nvl(earthquakeInfoArray[2 + 11 * i]));
-                pDTO.setTmEqkMsc(CmmUtil.nvl(earthquakeInfoArray[3 + 11 * i]));
-                pDTO.setMt(CmmUtil.nvl(earthquakeInfoArray[4 + 11 * i]));
-                pDTO.setLat(CmmUtil.nvl(earthquakeInfoArray[5 + 11 * i]));
-                pDTO.setLon(CmmUtil.nvl(earthquakeInfoArray[6 + 11 * i]));
-                pDTO.setLoc(CmmUtil.nvl(earthquakeInfoArray[7 + 11 * i]));
-                pDTO.setScale(CmmUtil.nvl(earthquakeInfoArray[8 + 11 * i]));
-                pDTO.setRem(CmmUtil.nvl(earthquakeInfoArray[9 + 11 * i].replaceAll("\\\\n", "")));
-                pDTO.setCor(CmmUtil.nvl(earthquakeInfoArray[10 + 11 * i]));
-
-                log.info("-----------------------------------");
-                log.info("TP : " + pDTO.getTp());
-                log.info("TM_FC : " + pDTO.getTmFc());
-                log.info("SEQ : " + pDTO.getSeq());
-                log.info("TmEqkMsk : " + pDTO.getTmEqkMsc());
-                log.info("MT : " + pDTO.getMt());
-                log.info("LAT : " + pDTO.getLat());
-                log.info("LON : " + pDTO.getLon());
-                log.info("LOC : " + pDTO.getLoc());
-                log.info("SCALE : " + pDTO.getScale());
-                log.info("REM : " + pDTO.getRem());
-                log.info("COR : " + pDTO.getCor());
-
+                // FIXME api json 키 값은 cctvname (카멜 케이스가 아닌데)
+                // dto에서는 카멜식임
+                pDTO.setCnt(node.path("cnt").asText());
+                pDTO.setFcTp(node.path("fcTp").asText());
+                pDTO.setImg(node.path("img").asText());
+                pDTO.setInT(node.path("inT").asText());
+                pDTO.setLat(node.path("lat").asText());
+                pDTO.setLoc(node.path("loc").asText());
+                pDTO.setLon(node.path("lon").asText());
+                pDTO.setMt(node.path("mt").asText());
+                pDTO.setRem(node.path("rem").asText());
+                pDTO.setStnId(node.path("stnId").asText());
+                pDTO.setTmEqk(node.path("tmEqk").asText());
+                pDTO.setTmFc(node.path("tmFc").asText());
+                pDTO.setTmSeq(node.path("tmSeq").asText());
+                pDTO.setDep(node.path("dep").asText());
 
                 earthquakeMapper.insertEarthquakeLiveInfo(pDTO);
 
-                pList.add(pDTO);
             }
         }
-        List<EarthquakeLiveDTO> rList = earthquakeMapper.getEarthquakeLiveInfo();
 
-        log.info(this.getClass().getName() + ".getEarthquakeLiveInfo End!");
+        log.info(this.getClass().getName() + ".manageEarthquakeLiveInfo End!!");
 
-        return rList;
     }
 
-    /**
-     * 지진 과거 API 접근을 위한 URL 정보 생성 로직
-     */
-    
-    public void setEarthquakeUrl() throws Exception {
-
-        log.info(this.getClass().getName() + ".setEarthquakeUrl Start !");
+    @Scheduled(cron = "0 5 * * * *") // 매시 5분 0초마다 실행되게 스케줄러 설정
+    void insertEarthquakeLiveInfo() throws Exception {
+        log.info(this.getClass().getName() + ".insertEarthquakeLiveInfo Start!!");
 
         // 현재 시간을 가져옵니다.
         Date currentDate = new Date();
 
-        // 날짜 및 시간 형식을 설정합니다.
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmm");
+        // Calendar 객체를 생성하고 현재 날짜로 설정합니다.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
 
-        // 현재 시간을 원하는 형식으로 변환합니다.
-        String formattedDate = dateFormat.format(currentDate);
+        // 3일 전의 날짜를 계산합니다. ( 3일전까지의 데이터까지 조회하기 위해 파라미터값으로 3일전 날짜를 생성 )
+        calendar.add(Calendar.DAY_OF_MONTH, -3);
 
-        log.info("현재 시각 : " + formattedDate);
+        // 형식을 설정하고 문자열로 변환합니다. ( 문자열로 변환하여 파라미터값으로 사용가능하게 변경 )
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        String formattedCurrentDate = dateFormat.format(currentDate);
+        String threeDaysAgo = dateFormat.format(calendar.getTime());
 
-        // 과거 지진데이터가 있는 기준일로부터 현재 시각까지의 데이터를 수집하기 위한 URL 정보 생성
-        String frDate = "20180101";
-        String laDate = formattedDate;
-        String cntDiv = "Y";
-        String orderTy = "xml";
+        String pageNo = "1";
+        String numOfRows = "100";
+        String dataType = "json";
 
-        int i = 0;  // 수정된 부분: 무한루프를 돌지 않도록 조건 수정
-        while (i < 1) {
-            log.info("현재 i : " + i);
+        String apiParam = apiLiveURL + "?serviceKey=" + dataApiKey + "&numOfRows=" + numOfRows + "&pageNo=" + pageNo + "&dataType=" + dataType + "&fromTmFc=" + threeDaysAgo + "&toTmFc=" + formattedCurrentDate;
+        log.info("apiParam : " + apiParam);
 
-            String apiParam = "?frDate=" + frDate + "&laDate=" + laDate + "&cntDiv=" + cntDiv + "&orderTy=" + orderTy + "&authKey=" + apiKey;
-            log.info("apiParam : " + apiParam);
+        Map<String, String> head = new HashMap<>();
 
-            // 수정된 부분: API 호출과 결과 처리를 별도의 메서드로 분리
-            getEarthquakeInfo(apiParam);
+        String json = NetworkUtil.get(apiParam, head);
 
-            i++;
-        }
-        log.info(this.getClass().getName() + ".setEarthquakeUrl End !");
-    }
+        log.info("json : " + json);
 
-    /**
-     * 위에서 받은 URL 정보를 가지고 지진 과거 API 호출 후
-     * XML 파싱 후 DB에 담는 로직
-     */
-    
-    public void getEarthquakeInfo(String apiParam) {
-        try {
-            // URL 객체를 생성하고 웹 주소를 넣습니다.
-            URL url = new URL(apiURL + apiParam);
-            log.info(url.toString());
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(json);
 
-            // HttpURLConnection 객체를 생성합니다.
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            // 요청 방식을 GET으로 설정합니다.
-            urlConnection.setRequestMethod("GET");
-            log.info("url 요청 전달 완료");
+        // totalCount 값 가져오기 (asText() 메서드로 String으로 가져오기)
+        String totalCount = rootNode.path("response").path("body").path("totalCount").asText();
+        log.info("조회된 지진 실시간 데이터의 totalCount : " + totalCount);
 
-            // BufferedReader 객체를 생성하고 HttpURLConnection의 InputStream을 넣습니다.
-            try (InputStream inputStream = urlConnection.getInputStream();
-                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-                String line;
-                StringBuilder response = new StringBuilder();
+        // items 노드 가져오기
+        JsonNode itemsNode = rootNode.path("response").path("body").path("items").path("item");
 
-                // 서버로부터 받은 데이터를 줄단위로 읽습니다.
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
+        if (itemsNode.isArray()) {
+            for (JsonNode node : itemsNode) {
+                EarthquakeLiveDTO pDTO = new EarthquakeLiveDTO();
+
+                pDTO.setTmEqk(node.path("tmEqk").asText());
+
+                EarthquakeLiveDTO rDTO = earthquakeMapper.getExists(pDTO);
+
+                if (rDTO.getExistsYn().equals("N")) {
+
+                    pDTO.setCnt(node.path("cnt").asText());
+                    pDTO.setFcTp(node.path("fcTp").asText());
+                    pDTO.setImg(node.path("img").asText());
+                    pDTO.setInT(node.path("inT").asText());
+                    pDTO.setLat(node.path("lat").asText());
+                    pDTO.setLoc(node.path("loc").asText());
+                    pDTO.setLon(node.path("lon").asText());
+                    pDTO.setMt(node.path("mt").asText());
+                    pDTO.setRem(node.path("rem").asText());
+                    pDTO.setStnId(node.path("stnId").asText());
+                    pDTO.setTmFc(node.path("tmFc").asText());
+                    pDTO.setTmSeq(node.path("tmSeq").asText());
+                    pDTO.setDep(node.path("dep").asText());
+
+                    earthquakeMapper.insertEarthquakeLiveInfo(pDTO);
+
                 }
-
-                // API 응답 결과를 문자열로 변환
-                String resultXml = response.toString();
-                log.info("API 호출 결과 (시작태그는 <alert>): " + resultXml);
-
-                // XmlMapper를 사용하여 XML을 Java 객체로 변환
-                XmlMapper xmlMapper = new XmlMapper();
-
-//                List <LinkedHashMap<String, LinkedHashMap<String, String>>> resultData = xmlMapper.readValue(resultXml, typeRef);
-
-                // TypeReference를 사용하여 제네릭 타입 지정
-                TypeReference<List<Object>> typeRef = new TypeReference<List<Object>>() {};
-                List<Object> resultData = xmlMapper.readValue(resultXml, typeRef);
-                log.info("rd : " + resultData);
-
-                List<Map<String, String>> dataList = new ArrayList<>();
-
-                // 결과 데이터를 가공하여 필요한 정보 추출
-                for (Object item : resultData) {
-                    if (item instanceof LinkedHashMap) {
-                        LinkedHashMap<String, Object> itemMap = (LinkedHashMap<String, Object>) item;
-
-                        for (Map.Entry<String, Object> entry : itemMap.entrySet()) {
-                            if ("info".equals(entry.getKey()) && entry.getValue() instanceof ArrayList) {
-                                ArrayList<LinkedHashMap<String, String>> items = (ArrayList<LinkedHashMap<String, String>>) entry.getValue();
-
-                                for (LinkedHashMap<String, String> infoMap : items) {
-                                    // 이제 infoMap에서 필요한 정보를 추출하여 Map에 담을 수 있습니다.
-                                    Map<String, String> dataMap = new HashMap<>(infoMap);
-                                    dataList.add(dataMap);
-                                }
-                            }
-                        }
-                    }
-                }
-
-                log.info("dataList : " + dataList);
-
-                log.info("xmlMapper의 readValue 작업 완료");
-
-                log.info("resultData : " + resultData);
-                log.info("생성된 DTO의 개수 : " + resultData.size());
-
-                List<EarthquakeResultDTO> erList = new ArrayList<>();
-
-                // DTO로 변환하여 DB에 저장
-                for (Map<String, String> resultDTO : dataList) {
-
-                    EarthquakeResultDTO erDTO = new EarthquakeResultDTO();
-
-                    log.info("resultDTO : " + resultDTO);
-
-                    erDTO.setMsgCode(resultDTO.get("msgCode"));
-                    erDTO.setCntDiv(resultDTO.get("cntDiv"));
-                    erDTO.setArDiv(resultDTO.get("arDiv"));
-                    erDTO.setEqArCdNm(resultDTO.get("eqArCdNm"));
-                    erDTO.setEqPt(resultDTO.get("eqPt"));
-                    erDTO.setNkDiv(resultDTO.get("nkDiv"));
-                    erDTO.setTmIssue(resultDTO.get("tmIssue"));
-                    erDTO.setEqDate(resultDTO.get("eqDate"));
-                    erDTO.setMagMl(resultDTO.get("magMl"));
-                    erDTO.setMagDiff(resultDTO.get("magDiff"));
-                    erDTO.setEqDt(resultDTO.get("eqDt"));
-                    erDTO.setEqLt(resultDTO.get("eqLt"));
-                    erDTO.setEqLn(resultDTO.get("eqLn"));
-                    erDTO.setMajorAxis(resultDTO.get("majorAxis"));
-                    erDTO.setMinorAxis(resultDTO.get("minorAxis"));
-                    erDTO.setDepthDiff(resultDTO.get("depthDiff"));
-                    erDTO.setJdLoc(resultDTO.get("jdLoc"));
-                    erDTO.setJdLocA(resultDTO.get("jdLocA"));
-                    erDTO.setReFer(resultDTO.get("ReFer"));
-
-                    erList.add(erDTO);
-
-                    log.info("erDTO : " + erDTO);
-                    log.info("erList : " + erList);
-
-                    earthquakeMapper.insertEarthquakeInfo(erDTO);
-
-                    erDTO = null;
-                }
-
-                log.info("매핑된 EarthquakeResultDTO 리스트 : " + erList);
             }
-            // 예외 발생시 스택트레이스를 출력합니다.
-        } catch (Exception e) {
-            log.error("Error in processEarthquakeData: " + e.getMessage(), e);
         }
+
+        log.info(this.getClass().getName() + ".insertEarthquakeLiveInfo End!!");
+
     }
+
 }
